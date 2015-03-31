f73bea71c90ae4e150ce2f7cbe0421c71e474d55
qlloveyan <13026148179@163.com>
2015-03-31 17:18:39 +0800

    设计模式代码修改提交

---
+++ tianMap/src/test/java/tianMap/T.java
@@ -0,0 +1,14 @@
+package tianMap;
+
+public class T {
+
+	public static void main(String[] args) {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
