6de9cb8ad7519f1e86d1460d974680a34e00aeb8
qlloveyan <13026148179@163.com>
2016-03-18 21:02:15 +0800

    适配器学习代码修改提交

---
+++ my_pro/src/main/java/com/surfilter/self/jse/designer/adapter/ClassAdapterFactory.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:my_pro
+ * File Name:AdapterFactory.java
+ * Package Name:com.surfilter.self.jse.designer
+ * Date:2016年3月18日下午8:39:57
+ *
+*/
+
+package com.surfilter.self.jse.designer.adapter;
+/**
+ * ClassName:AdapterFactory <br/>
+ * Function: 类的适配器模式学习. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016年3月18日 下午8:39:57 <br/>
+ * @author   quanli
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class ClassAdapterFactory {
+	//类的适配器
+	
+}
+
