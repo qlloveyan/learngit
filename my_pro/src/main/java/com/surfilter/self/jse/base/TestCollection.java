7bf271562d2eef25b1d8886fbf65e71508d86820
qlloveyan <13026148179@163.com>
2016-04-01 20:32:59 +0800

    基础知识学习修改

---
+++ my_pro/src/main/java/com/surfilter/self/jse/base/TestCollection.java
@@ -0,0 +1,29 @@
+/**
+ * Project Name:my_pro
+ * File Name:TestCollection.java
+ * Package Name:com.surfilter.self.jse.base
+ * Date:2016年4月1日下午7:27:03
+ *
+*/
+
+package com.surfilter.self.jse.base;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * ClassName:TestCollection <br/>
+ * Function: 集合类使用学习. <br/>
+ * Date:     2016年4月1日 下午7:27:03 <br/>
+ * @author   quanli
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class TestCollection {
+
+	public static void main(String[] args) {
+		Collection<String> strColle = new ArrayList<String>();
+	}
+}
+
