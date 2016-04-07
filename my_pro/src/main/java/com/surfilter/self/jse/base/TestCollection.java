aa84ae525881c2c3dab5d1ca69e49ad92d98fc22
qlloveyan <13026148179@163.com>
2016-04-07 12:12:10 +0800

    java基础学习提交

--- my_pro/src/main/java/com/surfilter/self/jse/base/TestCollection.java
+++ my_pro/src/main/java/com/surfilter/self/jse/base/TestCollection.java
@@ -9,68 +9,88 @@
 package com.surfilter.self.jse.base;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.RandomAccess;
-import java.util.logging.Logger;
+import java.util.Map;
 
 /**
  * ClassName:TestCollection <br/>
  * Function: 集合类使用学习. <br/>
- * Date:     2016年4月1日 下午7:27:03 <br/>
- * @author   quanli
- * @version  
- * @since    JDK 1.6
- * @see 	 
+ * Date: 2016年4月1日 下午7:27:03 <br/>
+ * 
+ * @author quanli
+ * @version
+ * @since JDK 1.6
+ * @see
  */
 public class TestCollection {
 
 	public static void main(String[] args) {
-		 long start = System.currentTimeMillis();
-		 ArrayList list = new ArrayList();
-		 Object obj = new Object();
-		 for(int i=0;i<5000000;i++){
-		 list.add(obj);
+//		 long start = System.currentTimeMillis();
+//		 ArrayList list = new ArrayList();
+//		 Object obj = new Object();
+//		 for(int i=0;i<5000000;i++){
+//			 list.add(obj);
+//		 }
+//		 long end = System.currentTimeMillis();
+//		 System.out.println(end-start);
+//
+//		 start = System.currentTimeMillis();
+//		 LinkedList list1 = new LinkedList();
+//		 Object obj1 = new Object();
+//		 for(int i=0;i<5000000;i++){
+//			 list1.add(obj1);
+//		 }
+//		 end = System.currentTimeMillis();
+//		 System.out.println(end-start);
+//
+//		 start = System.currentTimeMillis();
+//		 Object obj2 = new Object();
+//		 for(int i=0;i<1000;i++){
+//			 list.add(0,obj2);
+//		 }
+//		 end = System.currentTimeMillis();
+//		 System.out.println(end-start);
+//
+//		 start = System.currentTimeMillis();
+//		 Object obj3 = new Object();
+//		 for(int i=0;i<1000;i++){
+//			 list1.add(obj1);
+//		 }
+//		 end = System.currentTimeMillis();
+//		 System.out.println(end-start);
+//
+//		 start = System.currentTimeMillis();
+//		 list.remove(0);
+//		 end = System.currentTimeMillis();
+//		 System.out.println(end-start);
+//
+//		 start = System.currentTimeMillis();
+//		 list1.remove(250000);
+//		 end = System.currentTimeMillis();
+//		 System.out.println(end-start);
+		 
+		 //集合遍历 List Set
+		 List<String> list = new ArrayList<String>();
+		 list.add("1");
+		 list.add("2");
+		 list.add("3");
+		 
+		 Iterator<String> iter = list.iterator();
+		 while(iter.hasNext()){
+			 System.out.println(iter.next());
 		 }
-		 long end = System.currentTimeMillis();
-		 System.out.println(end-start);
-
-		 start = System.currentTimeMillis();
-		 LinkedList list1 = new LinkedList();
-		 Object obj1 = new Object();
-		 for(int i=0;i<5000000;i++){
-		 list1.add(obj1);
+		 
+		 //Map遍历
+		 Map<String,String> map = new HashMap<String,String>();
+		 map.put("1", "1");
+		 map.put("2", "2");
+		 map.put("3", "3");
+		 
+		 for(String key : map.keySet()){
+			 System.out.println(key+": " + map.get(key));
 		 }
-		 end = System.currentTimeMillis();
-		 System.out.println(end-start);
-
-		 start = System.currentTimeMillis();
-		 Object obj2 = new Object();
-		 for(int i=0;i<1000;i++){
-		 list.add(0,obj2);
-		 }
-		 end = System.currentTimeMillis();
-		 System.out.println(end-start);
-
-		 start = System.currentTimeMillis();
-		 Object obj3 = new Object();
-		 for(int i=0;i<1000;i++){
-		 list1.add(obj1);
-		 }
-		 end = System.currentTimeMillis();
-		 System.out.println(end-start);
-
-		 start = System.currentTimeMillis();
-		 list.remove(0);
-		 end = System.currentTimeMillis();
-		 System.out.println(end-start);
-
-		 start = System.currentTimeMillis();
-		 list1.remove(250000);
-		 end = System.currentTimeMillis();
-		 System.out.println(end-start);
-		
+		 
 	}
 }
-
--- my_pro/src/main/java/com/surfilter/self/jse/base/TestThreadPool.java
+++ my_pro/src/main/java/com/surfilter/self/jse/base/TestThreadPool.java
@@ -10,6 +10,7 @@ package com.surfilter.self.jse.base;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * ClassName:TestThreadPool <br/>
@@ -28,6 +29,8 @@ public class TestThreadPool {
 	 */
 	public static void main(String[] args) {
 		
+		TestThreadPool tt = new TestThreadPool();
+		
 		ExecutorService es = null;
 		/**
 		 * 1、newCachedThreadPool:创建一个自适应的线程池
@@ -41,6 +44,28 @@ public class TestThreadPool {
 		 * 3、newSingleThreadExecutor:创建一个单一线程
 		 */
 		es = Executors.newSingleThreadExecutor();
+		
+		//常见用法说
+		Thread t = new Thread(tt.new MyThread("测试线程!"));
+		es.submit(t);
+		es.shutdown();
+		
+		//在这里介绍 java.util.concurrent.atomic下的封装类,例如AtomicInteger
+		AtomicInteger ai = new AtomicInteger();
+		ai.set(2);//设置一个整型值
+		System.out.println(ai.get());//输出2
+		ai.compareAndSet(3, 4);//参数：(预期值,修改值)通过比较预期值与原值是否相同,相同则设置修改值
+		System.out.println(ai.get());//输出2
+		ai.compareAndSet(2, 4);
+		System.out.println(ai.get());//输出4
+		ai.getAndAdd(4);//取值并相加
+		System.out.println(ai.get());//输出8
+		
+		/**
+		 * 上面的功能可以理解为一个比较高级的计数器,为什么不用count++或++count的方式去实现,
+		 * 原因在与多线程高并发的情况下,传统的计数器在处理此情况下会存在线程并发问题,java.util.concurrent.atomic下封装的类,已经比较好的
+		 * 支持了高并发的情况,不需要自己去实现,因此在多线程的情况下,如果有用到这种情况的功能,建议采用该包下的封装类
+		 */
 	}
 	
 	class MyThread implements Runnable{
