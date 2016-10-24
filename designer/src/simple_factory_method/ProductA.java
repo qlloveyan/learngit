18161c63c93be72d50c2080bf18f9e7768108086
qlloveyan <13026148179@163.com>
2016-10-24 19:21:50 +0800

    设计模式重置学习

--- designer/src/simple_factory_method/ProductA.java
+++
@@ -1,8 +0,0 @@
-package simple_factory_method;
-
-public class ProductA {
-
-	public void say(){
-		System.out.println("我是A产品……");
-	}
-}
--- designer/src/simple_factory_method/ProductB.java
+++
@@ -1,8 +0,0 @@
-package simple_factory_method;
-
-public class ProductB {
-
-	public void say(){
-		System.out.println("我是B产品……");
-	}
-}
